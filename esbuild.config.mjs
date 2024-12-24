993f75e977bc23dfce43bad98b9257854b176f4b
BloodRedCodr <bloodreddev@icloud.com>
2024-12-24 16:00:19 +0530

    file rename

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -32,43 +32,28 @@ const esbuildOptions = {
 const build = async () => {
   await esbuild.build(esbuildOptions);
 
-  ncp.ncp('./src/index.html', './dist/index.html', (err) => {
+  ncp.ncp('./src/index.html', './dist/index.html', async (err) => {
     if (err) {
       return console.error('Error copying index.html:', err);
     }
 
     console.log('index.html copied to dist');
+
+    if (isDev) {
+      let ctx = await esbuild.context(esbuildOptions);
+  
+      let { host, port } = await ctx.serve({
+        servedir: 'dist',
+      });
+      console.log(`server running on ${host}:${port}`);
+    
+      await ctx.watch();
+      console.log('watching...');
+    }  
   });
 };
 
-const serve = async () => {
-  let ctx = await esbuild.context(esbuildOptions);
-
-  let { host, port } = await ctx.serve({
-    servedir: 'dist',
-  });
-  console.log(`server running on ${host}:${port}`);
-
-  ncp.ncp('./src/index.html', './dist/index.html', (err) => {
-    if (err) {
-      return console.error('Error copying index.html:', err);
-    }
-
-    console.log('index.html copied to dist');
-  });
-
-  await ctx.watch();
-  console.log('watching...');
-};
-
-if (isDev) {
-  serve().catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
-} else {
-  build().catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
-}
+build().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
