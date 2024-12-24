206dd20f3a8bdd954283e42e101de7763a49c349
BloodRedCodr <bloodreddev@icloud.com>
2024-12-24 11:19:15 +0530

    prettier + husky setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import { fixupPluginRules } from "@eslint/compat";
-import tsParser from "@typescript-eslint/parser";
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import { fixupPluginRules } from '@eslint/compat';
+import tsParser from '@typescript-eslint/parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,48 +18,56 @@ when below sharable configs are officially migrated to flat config format
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
+  allConfig: js.configs.all,
 });
 
 export default [
   ...compat.extends(
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ),
   {
     plugins: {
       react,
-      "react-hooks": fixupPluginRules(reactHooks),
-      "@typescript-eslint": typescriptEslint,
+      'react-hooks': fixupPluginRules(reactHooks),
+      '@typescript-eslint': typescriptEslint,
+      prettier,
     },
     languageOptions: {
       parser: tsParser,
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     rules: {
-      "react/prop-types": "off",
-      "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off",
-      "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-      "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
-      "max-len": [1, {
-        code: 120,
-        ignoreComments: true,
-        ignoreUrls: true,
-        ignoreRegExpLiterals: true,
-      }],
-      "max-lines": [1, {
-        max: 300,
-        skipBlankLines: true,
-        skipComments: true,
-      }],
+      'react/prop-types': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+      'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+      'max-len': [
+        1,
+        {
+          code: 120,
+          ignoreComments: true,
+          ignoreUrls: true,
+          ignoreRegExpLiterals: true,
+        },
+      ],
+      'max-lines': [
+        1,
+        {
+          max: 300,
+          skipBlankLines: true,
+          skipComments: true,
+        },
+      ],
     },
   },
 ];
