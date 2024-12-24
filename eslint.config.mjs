737f5c32382183309b682592f85cd906fb63f59c
BloodRedCodr <bloodreddev@icloud.com>
2024-12-24 10:04:17 +0530

    ESLint config with typescript support

---
+++ eslint.config.mjs
@@ -0,0 +1,64 @@
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import { fixupPluginRules } from "@eslint/compat";
+import tsParser from "@typescript-eslint/parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+/*
+TODO: remove FlatCompat and dependency @eslint/eslintrc,
+when below sharable configs are officially migrated to flat config format
+*/
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ),
+  {
+    plugins: {
+      react,
+      "react-hooks": fixupPluginRules(reactHooks),
+      "@typescript-eslint": typescriptEslint,
+    },
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/prop-types": "off",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+      "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+      "max-len": [1, {
+        code: 120,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreRegExpLiterals: true,
+      }],
+      "max-lines": [1, {
+        max: 300,
+        skipBlankLines: true,
+        skipComments: true,
+      }],
+    },
+  },
+];
